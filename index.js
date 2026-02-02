86f51b92f38a4506f7a099278279df4a5abea901
Erik <65229112+EjayGit@users.noreply.github.com>
2026-02-02 15:10:57 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+//config cors
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`This app is running on port ${PORT}`));
+
+let events = [
+  { id: 1, title: '"Red-eyed Tree Frog"', src: 'https://i.natgeofe.com/k/8fa25ea4-6409-47fb-b3cc-4af8e0dc9616/red-eyed-tree-frog-on-leaves-3-2_3x4.jpg', alt: 'An image of a red-eyed tree frog sitting on leaves.' },
+  { id: 2, title: 'A smiley frog', src: 'https://as1.ftcdn.net/v2/jpg/01/99/77/64/1000_F_199776474_ORr8LaAj6pihYKMD4HqHpQ2nkTZ0IrIX.jpg', alt: 'An image of a frog that is smiling happily.' },
+  { id: 3, title: 'A Poison Dart Frog', src: 'https://cdn.britannica.com/73/100273-050-221A0593/arrow-poison-frogs-sound-production-colour-warning-signal.jpg', alt: 'An image of a colourful poison-dart frog.' },
+  { id: 4, title: 'Three cute froglets', src: 'https://kids.earth.org/wp-content/uploads/2023/07/Untitled-683-%C3%97-1024px-1024-%C3%97-683px-2023-07-26T184215.475-900x600.jpg', alt: 'An image of three small frogs on a branch' },
+  { id: 5, title: 'A chonky green frog', src: 'https://i0.wp.com/www.australiangeographic.com.au/wp-content/uploads/2018/06/Litoria_caerulea_-_Darwin_NT.jpg?fit=1000%2C672&ssl=1', alt: 'An image of a very large shiny green frog.' },
+  { id: 6, title: 'A chonky green frog', src: 'https://i0.wp.com/www.australiangeographic.com.au/wp-content/uploads/2018/06/Litoria_caerulea_-_Darwin_NT.jpg?fit=1000%2C672&ssl=1', alt: 'An image of a very large shiny green frog.' }
+];
+
+app.get('/events', (req, res) => {
+  res.json(events);
+});
